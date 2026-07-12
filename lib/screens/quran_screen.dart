746c02bb8b4c410093a4a2ed0db3ea127b1709a9
Alan Baimukhan <alanbaimukhan@users.noreply.github.com>
2026-07-12 13:59:20 +0500

    Fix Quran screen provider import

--- lib/screens/quran_screen.dart
+++ lib/screens/quran_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../models/quran.dart';
